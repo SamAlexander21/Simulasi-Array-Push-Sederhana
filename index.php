ac9d399f0e496a84c38d492dabb5f47d599ade25
alsocube <obongs221@gmail.com>
2025-05-24 00:09:08 +0700

    nek error njedul tulisan + nek wes login redirect neng userDash.php

--- index.php
+++ index.php
@@ -9,14 +9,16 @@
 <body>
 <?php
 session_start();
+    if (isset($_SESSION['username'])) {
+        header('Location: userDash.php');
+    }
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = array('2313010627', '2313010628', '2313010639', '2313010642', '2313010651');
         $password = array('2313010627', '2313010628', '2313010639', '2313010642', '2313010651');
 
         if (in_array($_POST['username'], $username) && in_array($_POST['password'], $password)) {
             if ($_POST['username'] != $_POST['password']) {
-                echo "<script>alert('Username dan password tidak sesuai!');</script>";
-                // exit();
+                echo "";
             } else {
                 switch ($_POST['username']) {
                     case '2313010627':
@@ -45,12 +47,22 @@ session_start();
                 }
             }
         } else {
-            echo "Username atau password salah!";
+            class err {
+                public function errorLogin() {
+                    echo "<h3 style='text-align:center;'>Username atau Password Salah!</h3>";
+                }
+            }
         }
     }
 ?>
 <?php include 'navbar.php'; ?>
     <div class="container">
+        <?php
+            if ($_POST && (!in_array($_POST['username'], $username) || !in_array($_POST['password'], $password))) {
+                $err = new err();
+                $err->errorLogin();
+            }
+        ?>
         <form action="" method="post">
         <label for="username">Username :</label>
         <input type="text" name="username" id="username" required>
