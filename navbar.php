e29137661897554aaa4c926f71eef30dcf7d0538
alsocube <obongs221@gmail.com>
2025-06-10 00:06:32 +0700

    nambah2i gen ra aneh2

--- navbar.php
+++ navbar.php
@@ -10,23 +10,34 @@
                 echo "<img class='pp' src='assets/".$_SESSION['username'].".png' alt='foto wajah'>";
                 echo "<a href='userDash.php'>" . $_SESSION['username'] . "</a>"; 
                 echo "</div>";
-            } else if ($_SERVER['PHP_SELF']) {
+            } else if (basename($_SERVER['PHP_SELF']) == 'index.php') {
                 echo " ";
-            }
+            } else if ($_SERVER['PHP_SELF']) {
+                echo "<div class='user'>";
+                echo "<img class='pp' src='' alt='foto wajah'>";
+                echo "<a href='userDash.php'>Error</a>";
+                echo "</div>";
+            } 
         ?>
         <div class="nav-logo">
             <a href="index.php">Inventori Toko</a>
         </div>
         <?php
-            if (isset($_SESSION['username'], $_SESSION['password'])) {
+            if (isset($_SESSION['username'], $_SESSION['password']) && basename($_SERVER['PHP_SELF']) == $_SESSION['username'].'.php') {
                 echo "<div class='nav-links'>";
-                echo "<a href='".$_SESSION['username'].".php'>"."Dashboard</a>";
                 echo "<a href='logout.php'>Logout</a>";
                 echo "</div>";
-            } else if (basename($_SERVER['PHP_SELF']) != 'index.php') {
-                echo "<a href='index.php'>Login</a>";
-            } else if ($_SERVER['PHP_SELF']) {
+            } else if (basename($_SERVER['PHP_SELF']) == 'userDash.php') {
+                echo "<div class='nav-links'>";
+                echo "<a href='".$_SESSION['username'].".php'>"."Form</a>";
+                echo "<a href='logout.php'>Logout</a>";
+                echo "</div>";
+            } else if (basename($_SERVER['PHP_SELF']) == 'index.php') {
                 echo " ";
+            } else if ($_SERVER['PHP_SELF']) {
+                echo "<div class='nav-links'>";
+                echo "<a href='index.php'>Login</a>";
+                echo "</div>";
             }
 
             ?>
